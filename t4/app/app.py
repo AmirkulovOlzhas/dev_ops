24e03534c8214a0f07658db70a8d5e44ac10a784
Olzhas Amirkulov <vip0amirkulov@gmail.com>
2023-10-22 18:53:43 +0600

    task 4

--- t4/app/app.py
+++ t4/app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from forms import LoginForm
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@db:5432/postgres'
@@ -7,19 +9,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@db:5432/
 db = SQLAlchemy(app)
 
 class User(db.Model):
+  __tablename__ = 'users'
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(255))
   email = db.Column(db.String(255))
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 
 @app.route('/users')
 def users():
   users = User.query.all()
-
-  # Преобразуем полученные данные в список словарей
 
   users_list = []
   for user in users:
@@ -29,9 +30,20 @@ def users():
       'email': user.email,
     })
 
-  # Отображаем данные в шаблоне Flask
+  return render_template('users.html', users=users_list)
 
-  return render_template('./app/users.html', users=users_list)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', 
+        title = 'Sign In',
+        form = form)
+
+
+@app.route("/user/<string:name>/<int:id>")
+def user(name, id):
+    return "User page: " + name + " - " + str(id)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port =int("3000"),  debug=True)
